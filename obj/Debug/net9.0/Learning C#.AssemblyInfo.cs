edd8cbdfc66b153fd0a90d1aaa315502167a1c8a
MuhammadAlaminMir <alaminmir08@gmail.com>
2025-04-12 10:32:21 +0600

    ..

--- obj/Debug/net9.0/Learning C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/Learning C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06ae5c0db941429a446bc57c1023d667d20ff5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89865edc37c134d866a5dec2bb85036fca4361aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
